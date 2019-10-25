518711aae5e908ce849d18be6a931cf87e6bbcd5
Jeremy Cheung <z5142254@ad.unsw.edu.au>
2019-10-25 16:06:00 +1100

    updated part3

--- hw1/src/part3.py
+++ hw1/src/part3.py
@@ -20,216 +20,229 @@ import numpy as np
 
 
 class Linear(nn.Module):
-    """
-    DO NOT MODIFY
-    Linear (10) -> ReLU -> LogSoftmax
-    """
+	"""
+	DO NOT MODIFY
+	Linear (10) -> ReLU -> LogSoftmax
+	"""
 
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(784, 10)
+	def __init__(self):
+		super().__init__()
+		self.fc1 = nn.Linear(784, 10)
 
-    def forward(self, x):
-        x = x.view(x.shape[0], -1)  # make sure inputs are flattened
+	def forward(self, x):
+		x = x.view(x.shape[0], -1)  # make sure inputs are flattened
 
-        x = F.relu(self.fc1(x))
-        x = F.log_softmax(x, dim=1)  # preserve batch dim
+		x = F.relu(self.fc1(x))
+		x = F.log_softmax(x, dim=1)  # preserve batch dim
 
-        return x
+		return x
 
 
 class FeedForward(nn.Module):
-    """
-    TODO: Implement the following network structure
-    Linear (256) -> ReLU -> Linear(64) -> ReLU -> Linear(10) -> ReLU-> LogSoftmax
-    """
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(784, 256)
-        self.fc2 = nn.Linear(256, 64)
-        self.fc3 = nn.Linear(64, 10)
-    def forward(self, x):
-        x = x.view(x.shape[0], -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.log_softmax(x, dim=1)
-        
-        return x
+	"""
+	TODO: Implement the following network structure
+	Linear (256) -> ReLU -> Linear(64) -> ReLU -> Linear(10) -> ReLU-> LogSoftmax
+	"""
+	def __init__(self):
+		super().__init__()
+		self.fc1 = nn.Linear(784, 256)
+		self.fc2 = nn.Linear(256, 64)
+		self.fc3 = nn.Linear(64, 10)
+	def forward(self, x):
+		x = x.view(x.shape[0], -1)
+		x = F.relu(self.fc1(x))
+		x = F.relu(self.fc2(x))
+		x = F.relu(self.fc3(x))
+		x = F.log_softmax(x, dim=1)
+		
+		return x
 class CNN(nn.Module):
-    """
-    TODO: Implement CNN Network structure
+	"""
+	TODO: Implement CNN Network structure
 
-    conv1 (channels = 10, kernel size= 5, stride = 1) -> Relu -> max pool (kernel size = 2x2) ->
-    conv2 (channels = 50, kernel size= 5, stride = 1) -> Relu -> max pool (kernel size = 2x2) ->
-    Linear (256) -> Relu -> Linear (10) -> LogSoftmax
+	conv1 (channels = 10, kernel size= 5, stride = 1) -> Relu -> max pool (kernel size = 2x2) ->
+	conv2 (channels = 50, kernel size= 5, stride = 1) -> Relu -> max pool (kernel size = 2x2) ->
+	Linear (256) -> Relu -> Linear (10) -> LogSoftmax
 
 
-    Hint: You will need to reshape outputs from the last conv layer prior to feeding them into
-    the linear layers.
-    """
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size = 5, stride = 1)
-        self.conv2 = nn.Conv2d(10, 50, kernel_size = 5, stride = 1)
-        self.pool = nn.MaxPool2d(kernel_size = 2)
-        self.fc1 = nn.Linear(800, 256)
-        self.fc2 = nn.Linear(256, 10)
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 800)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        x = F.log_softmax(x, dim=1)
-        return x
-    
+	Hint: You will need to reshape outputs from the last conv layer prior to feeding them into
+	the linear layers.
+	"""
+	def __init__(self):
+		super().__init__()
+		self.conv1 = nn.Conv2d(1, 10, kernel_size = 5, stride = 1)
+		self.conv2 = nn.Conv2d(10, 50, kernel_size = 5, stride = 1)
+		self.pool = nn.MaxPool2d(kernel_size = 2)
+		self.fc1 = nn.Linear(800, 256)
+		self.fc2 = nn.Linear(256, 10)
+	def forward(self, x):
+		x = self.pool(F.relu(self.conv1(x)))
+		x = self.pool(F.relu(self.conv2(x)))
+		x = x.view(-1, 800)
+		x = F.relu(self.fc1(x))
+		x = self.fc2(x)
+		x = F.log_softmax(x, dim=1)
+		return x
+	
 
 
 class NNModel:
-    def __init__(self, network, learning_rate):
-        """
-        Load Data, initialize a given network structure and set learning rate
-        DO NOT MODIFY
-        """
+	def __init__(self, network, learning_rate):
+		"""
+		Load Data, initialize a given network structure and set learning rate
+		DO NOT MODIFY
+		"""
 
-        # Define a transform to normalize the data
-        transform = transforms.Compose([transforms.ToTensor(),
-                                        transforms.Normalize((0.5,), (0.5,))])
+		# Define a transform to normalize the data
+		transform = transforms.Compose([transforms.ToTensor(),
+										transforms.Normalize((0.5,), (0.5,))])
 
-        # Download and load the training data
-        trainset = datasets.KMNIST(root='./data', train=True, download=True, transform=transform)
-        self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=False)
+		# Download and load the training data
+		trainset = datasets.KMNIST(root='./data', train=True, download=True, transform=transform)
+		self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=False)
 
-        # Download and load the test data
-        testset = datasets.KMNIST(root='./data', train=False, download=True, transform=transform)
-        self.testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
+		# Download and load the test data
+		testset = datasets.KMNIST(root='./data', train=False, download=True, transform=transform)
+		self.testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
 
-        self.model = network
+		self.model = network
 
-        """
-        TODO: Set appropriate loss function such that learning is equivalent to minimizing the
-        cross entropy loss. Note that we are outputting log-softmax values from our networks,
-        not raw softmax values, so just using torch.nn.CrossEntropyLoss is incorrect.
-        
-        Hint: All networks output log-softmax values (i.e. log probabilities or.. likelihoods.). 
-        """
-        self.lossfn = None
-        self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
+		"""
+		TODO: Set appropriate loss function such that learning is equivalent to minimizing the
+		cross entropy loss. Note that we are outputting log-softmax values from our networks,
+		not raw softmax values, so just using torch.nn.CrossEntropyLoss is incorrect.
+		
+		Hint: All networks output log-softmax values (i.e. log probabilities or.. likelihoods.). 
+		"""
+		self.lossfn = torch.nn.NLLLoss()
+		self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
 
-        self.num_train_samples = len(self.trainloader)
-        self.num_test_samples = len(self.testloader)
+		self.num_train_samples = len(self.trainloader)
+		self.num_test_samples = len(self.testloader)
 
-    def view_batch(self):
-        """
-        TODO: Display first batch of images from trainloader in 8x8 grid
+	def view_batch(self):
+		"""
+		TODO: Display first batch of images from trainloader in 8x8 grid
 
-        Do not make calls to plt.imshow() here
+		Do not make calls to plt.imshow() here
 
-        Return:
-           1) A float32 numpy array (of dim [28*8, 28*8]), containing a tiling of the batch images,
-           place the first 8 images on the first row, the second 8 on the second row, and so on
-           2) An int 8x8 numpy array of labels corresponding to this tiling
-        """
-        iterator = iter(self.trainloader)
-        images, labels = iterator.next()
-        batch = torch.empty(0)
-        
-        for i in range(0,8):
-            img = images.numpy()[i*8:(i+1)*8]
-            img = img.reshape(8,28,28)
-            imgnp = torch.from_numpy(img)
-            row = imgnp[0]
-            for j in range(1,8):
-                row = torch.cat((row, imgnp[j]),dim=1)
-            
-            batch = torch.cat((batch, row),dim=0)
-        
-        return batch, labels.view(8, 8)
+		Return:
+		   1) A float32 numpy array (of dim [28*8, 28*8]), containing a tiling of the batch images,
+		   place the first 8 images on the first row, the second 8 on the second row, and so on
+		   2) An int 8x8 numpy array of labels corresponding to this tiling
+		"""
+		iterator = iter(self.trainloader)
+		images, labels = iterator.next()
+		labels = labels.view(8, 8).numpy() 
+		row = torch.empty(0)
+		image = torch.empty(0)
+		x = 0
+		y = 0
+		k = 0
+		for images, label in self.trainloader:
+			for x in images:
+				if y < 8:
+					x = torch.Tensor.view(x, [28,28])
+					row = torch.cat([row,x],1)
+					y += 1
+				if y == 8: 
+					image = torch.cat([image,row],0)
+					row = torch.empty(0)
+					y = 0
+					k += 1
+				if k == 8:
+					break
+					
+			break
 
-    def train_step(self):
-        """
-        Used for submission tests and may be usefull for debugging
-        DO NOT MODIFY
-        """
-        self.model.train()
-        for images, labels in self.trainloader:
-            log_ps = self.model(images)
-            loss = self.lossfn(log_ps, labels)
 
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
-            return
 
-    def train_epoch(self):
-        self.model.train()
-        for images, labels in self.trainloader:
-            log_ps = self.model(images)
-            loss = self.lossfn(log_ps, labels)
+		
+		return image, labels
 
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
+	def train_step(self):
+		"""
+		Used for submission tests and may be usefull for debugging
+		DO NOT MODIFY
+		"""
+		self.model.train()
+		for images, labels in self.trainloader:
+			log_ps = self.model(images)
+			loss = self.lossfn(log_ps, labels)
 
-        return
+			self.optimizer.zero_grad()
+			loss.backward()
+			self.optimizer.step()
+			return
 
-    def eval(self):
-        self.model.eval()
-        accuracy = 0
-        with torch.no_grad():
-            for images, labels in self.testloader:
-                log_ps = self.model(images)
-                ps = torch.exp(log_ps)
-                top_p, top_class = ps.topk(1, dim=1)
-                equals = top_class == labels.view(*top_class.shape)
-                accuracy += torch.mean(equals.type(torch.FloatTensor))
+	def train_epoch(self):
+		self.model.train()
+		for images, labels in self.trainloader:
+			log_ps = self.model(images)
+			loss = self.lossfn(log_ps, labels)
 
-        return accuracy / self.num_test_samples
+			self.optimizer.zero_grad()
+			loss.backward()
+			self.optimizer.step()
+
+		return
+
+	def eval(self):
+		self.model.eval()
+		accuracy = 0
+		with torch.no_grad():
+			for images, labels in self.testloader:
+				log_ps = self.model(images)
+				ps = torch.exp(log_ps)
+				top_p, top_class = ps.topk(1, dim=1)
+				equals = top_class == labels.view(*top_class.shape)
+				accuracy += torch.mean(equals.type(torch.FloatTensor))
+
+		return accuracy / self.num_test_samples
 
 
 def plot_result(results, names):
-    """
-    Take a 2D list/array, where row is accuracy at each epoch of training for given model, and
-    names of each model, and display training curves
-    """
-    for i, r in enumerate(results):
-        plt.plot(range(len(r)), r, label=names[i])
-    plt.legend()
-    plt.title("KMNIST")
-    plt.xlabel("Epoch")
-    plt.ylabel("Test accuracy")
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-    plt.savefig("./part_2_plot.png")
+	"""
+	Take a 2D list/array, where row is accuracy at each epoch of training for given model, and
+	names of each model, and display training curves
+	"""
+	for i, r in enumerate(results):
+		plt.plot(range(len(r)), r, label=names[i])
+	plt.legend()
+	plt.title("KMNIST")
+	plt.xlabel("Epoch")
+	plt.ylabel("Test accuracy")
+	plt.grid(True)
+	plt.tight_layout()
+	plt.show()
+	plt.savefig("./part_2_plot.png")
 
 
 def main():
-    models = [Linear(), FeedForward(), CNN()]  # Change during development
-    epochs = 10
-    results = []
+	models = [Linear(), FeedForward(), CNN()]  # Change during development
+	epochs = 10
+	results = []
 
-    # Can comment the below out during development
-    images, labels = NNModel(Linear(), 0.003).view_batch()
-    print(labels)
-    plt.imshow(images, cmap="Greys")
-    plt.show()
+	# Can comment the below out during development
+	images, labels = NNModel(Linear(), 0.003).view_batch()
+	print(labels)
+	plt.imshow(images, cmap="Greys")
+	plt.show()
 
-    for model in models:
-        print(f"Training {model.__class__.__name__}...")
-        m = NNModel(model, 0.003)
+	for model in models:
+		print(f"Training {model.__class__.__name__}...")
+		m = NNModel(model, 0.003)
 
-        accuracies = [0]
-        for e in range(epochs):
-            m.train_epoch()
-            accuracy = m.eval()
-            print(f"Epoch: {e}/{epochs}.. Test Accuracy: {accuracy}")
-            accuracies.append(accuracy)
-        results.append(accuracies)
+		accuracies = [0]
+		for e in range(epochs):
+			m.train_epoch()
+			accuracy = m.eval()
+			print(f"Epoch: {e}/{epochs}.. Test Accuracy: {accuracy}")
+			accuracies.append(accuracy)
+		results.append(accuracies)
 
-    plot_result(results, [m.__class__.__name__ for m in models])
+	plot_result(results, [m.__class__.__name__ for m in models])
 
 
 if __name__ == "__main__":
-    main()
+	main()
